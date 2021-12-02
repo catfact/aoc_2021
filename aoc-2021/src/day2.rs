28d7dfcd51cc5f84c3b505b676c40efdf354927b
catfact <catfact@noreply.users.github.com>
2021-12-01 23:29:45 -0800

    day 2, test 2

--- aoc-2021/src/day2.rs
+++ aoc-2021/src/day2.rs
@@ -5,10 +5,10 @@ enum Dir {
     Forward, Up, Down, Invalid
 }
 
-fn parse_command_list(filename:&str) -> Vec<(Dir, i32)> { 
+fn parse_command_list(filename:&str) -> Vec<(Dir, i64)> { 
     let txt = fs::read_to_string(filename).expect("input failed!");
     let lines = txt.split("\n");
-    let mut commands: Vec<(Dir, i32)> = Vec::new();
+    let mut commands: Vec<(Dir, i64)> = Vec::new();
     for l in lines { 
         let cmd = l.split(" ").collect::<Vec<&str>>();
         let dir = match cmd[0].as_ref() {
@@ -17,19 +17,19 @@ fn parse_command_list(filename:&str) -> Vec<(Dir, i32)> {
             "down" => Dir::Down,
             _ => Dir::Invalid 
         };
-        let val = cmd[1].parse::<i32>();
+        let val = cmd[1].parse::<i64>();
         if val.is_ok() {
-            let com =  (dir, val.unwrap());
+            let com = (dir, val.unwrap());
             commands.push(com);
         } 
     }
     return commands;
 }
 
-pub fn test1() -> i32 { 
+pub fn test1() -> i64 { 
     let commands = parse_command_list("input/2.txt");
-    let mut x:i32 = 0;
-    let mut z:i32 = 0;
+    let mut x:i64 = 0;
+    let mut z:i64 = 0;
     for com in commands {
         match com.0 {
             Dir::Forward => { x = x + com.1; },
@@ -41,6 +41,25 @@ pub fn test1() -> i32 {
     return x * z;
 }
 
-pub fn test2() -> u32 { 
-    return 0;
+pub fn test2() -> i64 { 
+    let commands = parse_command_list("input/2.txt");
+    let mut x:i64 = 0;
+    let mut z:i64 = 0;
+    let mut a:i64 = 0;
+    for com in commands {
+        match com.0 {
+            Dir::Forward => { 
+                x = x + com.1;
+                z = z + (a * com.1);
+            },
+            Dir::Up => { 
+                a = a - com.1;
+            },
+            Dir::Down => {
+                a = a + com.1;
+            },
+            Dir::Invalid => {}
+        }
+    }
+    return x * z;
 }
--- aoc-2021/src/main.rs
+++ aoc-2021/src/main.rs
@@ -4,7 +4,7 @@ mod day2;
 fn main() {
     println!("day 1, test 1 result: {}", day1::test1());
     println!("day 1, test 2 result: {}", day1::test2());
- 
+    
     println!("day 2, test 1 result: {}", day2::test1());
-//    println!("day 2, test 2 result: {}", day2::test2());   
+    println!("day 2, test 2 result: {}", day2::test2());   
 }
